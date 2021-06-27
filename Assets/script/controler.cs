4dc6a7a1cd3ed285ea24e158a7e5745bba81351e
remiravat <remravat@gmail.com>
2021-06-27 19:06:50 +0200

    saut ok

---
+++ Assets/script/controler.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class controler : MonoBehaviour
+{
+    [SerializeField]
+    private float speed ;
+    private Vector3 mvnt;
+    private Rigidbody2D rb;
+    private bool isJumping;
+    private bool isGrounded;
+    [SerializeField]
+    private LayerMask layerMask;
+    private float jumpingTime;
+    [SerializeField]
+    private AnimationCurve jumpCurve;
+
+    void Start()
+    {
+        
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+
+    void Update()
+    {
+        InputHandler();
+        Debug.Log(isGrounded);
+        
+    }
+    private void FixedUpdate()
+    {
+        JumpUpdate();
+        Mouve();
+    }
+
+    void InputHandler()
+    {
+        mvnt = new Vector3(Input.GetAxis("Horizontal"),-2,0);
+        if (Input.GetButtonDown("Jump"))
+        {
+            JumpStart();
+        }
+        if (Input.GetButtonUp("Jump"))
+        {
+            if (jumpingTime < 0.6f)
+            {
+                jumpingTime = 0.6f;
+            }
+        }
+    }
+
+    void Mouve()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.52f,layerMask);
+        if (hit.collider != null)
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+        rb.velocity = mvnt * speed * Time.fixedDeltaTime;
+    }
+    void JumpStart()
+    {
+        if (isJumping && !isGrounded)
+        {
+            return;
+        }
+        isJumping = true;
+        jumpingTime = 0;
+    }
+
+    void JumpUpdate()
+    {
+        if (!isJumping)
+        {
+            return;
+        }
+        jumpingTime += Time.fixedDeltaTime;
+        mvnt += new Vector3(0, jumpCurve.Evaluate(jumpingTime), 0);
+        if (jumpingTime>jumpCurve.keys[jumpCurve.keys.Length-1].time)
+        {
+            isJumping = false;
+        }
+
+    }
+   
+}
